47340264ef1a5e1d43dc6da6a2d9d16d95386cd7
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-09-23 20:15:23 +0500

    ..

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import streamlit as st
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import easyocr
-import numpy as np
 import cv2
+import numpy as np
+
 app = Flask(__name__)
 CORS(app)
 
